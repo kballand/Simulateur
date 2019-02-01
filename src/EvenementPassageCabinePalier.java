f4751c3738a7a138e500216eee89dd1d4114cf19
chevrier jc <chevrie44u@etu.univ-lorraine.fr>
2019-02-01 11:09:35 +0100

    Factorisation du code

--- src/EvenementPassageCabinePalier.java
+++ src/EvenementPassageCabinePalier.java
@@ -18,16 +18,33 @@ public class EvenementPassageCabinePalier extends Evenement {
 
     public void traiter(Immeuble immeuble, Echeancier echeancier) {
         Cabine cabine = immeuble.cabine;
+
+
         assert !cabine.porteOuverte;
         assert étage.numéro() != cabine.étage.numéro();
         assert cabine.intention() == '^' || cabine.intention() == 'v';
-        if((!cabine.étage.equals(immeuble.étageLePlusBas()) || cabine.intention() == '^') && (!cabine.étage.equals(immeuble.étageLePlusHaut()) || cabine.intention() == 'v')) {
+
+
+        if((!cabine.étage.equals(immeuble.étageLePlusBas()) || cabine.intention() == '^')
+           && (!cabine.étage.equals(immeuble.étageLePlusHaut()) || cabine.intention() == 'v'))
             cabine.étage = this.étage;
-        }
-        if(cabine.passagersVeulentDescendre() || étage.aDesPassagers()) {
-            echeancier.ajouter(new EvenementOuverturePorteCabine(this.date + Global.tempsPourOuvrirOuFermerLesPortes));
-        } else if((!this.étage.equals(immeuble.étageLePlusBas()) || cabine.intention() == '^') && (!this.étage.equals(immeuble.étageLePlusHaut()) || cabine.intention() == 'v')) {
-            echeancier.ajouter(new EvenementPassageCabinePalier(this.date + Global.tempsPourBougerLaCabineDUnEtage, immeuble.étage(this.étage.numéro() + (cabine.intention() == 'v' ? - 1 : 1))));
+
+
+        if(cabine.passagersVeulentDescendre() || étage.aDesPassagers())
+            echeancier.ajouter(
+                    new EvenementOuverturePorteCabine(
+                            date + Global.tempsPourOuvrirOuFermerLesPortes
+                    )
+            );
+        else{
+            if( (!this.étage.equals(immeuble.étageLePlusBas()) || cabine.intention() == '^')
+             && (!this.étage.equals(immeuble.étageLePlusHaut()) || cabine.intention() == 'v'))
+                echeancier.ajouter
+                    (new EvenementPassageCabinePalier(
+                            date + Global.tempsPourBougerLaCabineDUnEtage,
+                            immeuble.étage(étage.numéro() + (cabine.intention() == 'v' ? - 1 : 1))
+                    )
+                );
         }
     }
 }
